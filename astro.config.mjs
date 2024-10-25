771ac7dedb1806e44b3beab5216296b3b9c5ceb1
Russley Shaw <russleyshaw@gmail.com>
2024-10-24 22:57:25 -0500

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
